2441b5a6e9992946a6e2da01be33ccbd2cf4e51d
orefice-michele <91534750+orefice-michele@users.noreply.github.com>
2021-12-02 00:58:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#unclude "lib.h"
+void primo(int a, int i, int &conta){
+  int c=0;
+  i++;
+  if(a>1 and a>i) {
+    c=a %i;
+    if (c==0){
+      conta++;
+    }
+    else {
+      primo (a,i,conta);
+    }
+  }
+}
