0105b3ed806875941f3a759af3c7a3e53bcc8ce7
orefice-michele <91534750+orefice-michele@users.noreply.github.com>
2021-12-02 01:05:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int numero, conta=0;
+  cin>> numero;
+  primo(numero, 1, conta);
+  if (counta==0){
+    cont <<"numero primo"<<;
+  }
+  else{
+    cout<<"numero non primo"<<;
+  }
   return 0;
 }
